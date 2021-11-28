66baac9aae1f87cf200da0b297869ff3df90e7b6
TMachon <theo.machon.91@gmail.com>
2021-11-28 19:11:18 +0100

    RM tab3, rework tab1

---
+++ src/menuItems.js
@@ -0,0 +1,233 @@
+
+export const menuItems = [
+  {
+    "id": "1",
+    "item": "Soupe miso"
+  },
+  {
+    "id": "2",
+    "item": "Salade de choux"
+  },
+  {
+    "id": "3",
+    "item": "Raviolis japonais (x3)"
+  },
+  {
+    "id": "4",
+    "item": "Raviolis aux crevettes (x3)"
+  },
+  {
+    "id": "5",
+    "item": "Bouchées aux crevettes (x3)"
+  },
+  {
+    "id": "6",
+    "item": "Raviolis aux crevettes frits (x3)"
+  },
+  {
+    "id": "7",
+    "item": "Beignets de maki (x3)"
+  },
+  {
+    "id": "8",
+    "item": "Beignets de courgettes (x3)"
+  },
+  {
+    "id": "9",
+    "item": "Beignets de pince de crabe (x3)"
+  },
+  {
+    "id": "10",
+    "item": "Beignets de poisson (x3)"
+  },
+  {
+    "id": "11",
+    "item": "Beignets de noix de St-Jacques (x3)"
+  },
+  {
+    "id": "12",
+    "item": "Beignets de poulet (x3)"
+  },
+  {
+    "id": "13",
+    "item": "Nêms au poulet (x3)"
+  },
+  {
+    "id": "14",
+    "item": "Riz blanc"
+  },
+  {
+    "id": "15",
+    "item": "Riz vinaigré"
+  },
+  {
+    "id": "16",
+    "item": "Riz cantonais"
+  },
+  {
+    "id": "17",
+    "item": "Riz thaïlandais"
+  },
+  {
+    "id": "18",
+    "item": "Nouilles sautées"
+  },
+  {
+    "id": "19",
+    "item": "Yakitori poulet (x2)"
+  },
+  {
+    "id": "20",
+    "item": "Yakitori ailes de poulet (x2)"
+  },
+  {
+    "id": "21",
+    "item": "Yakitori boulettes de poulet (x2)"
+  },
+  {
+    "id": "22",
+    "item": "Yakitori bœuf (x2)"
+  },
+  {
+    "id": "23",
+    "item": "Yakitori bœuf au fromage (x2)"
+  },
+  {
+    "id": "24",
+    "item": "Yakitori champignons (x2)"
+  },
+  {
+    "id": "25",
+    "item": "Yakitori courgettes (x2)"
+  },
+  {
+    "id": "26",
+    "item": "Yakitori saumon (x2)"
+  },
+  {
+    "id": "27",
+    "item": "Sushi saumon (x2)"
+  },
+  {
+    "id": "28",
+    "item": "Sushi thon (x2)"
+  },
+  {
+    "id": "29",
+    "item": "Sushi daurade (x2)"
+  },
+  {
+    "id": "30",
+    "item": "Sushi crevette (x2)"
+  },
+  {
+    "id": "31",
+    "item": "Sushi  anguilles (x2)"
+  },
+  {
+    "id": "32",
+    "item": "Temaki crevette avocat"
+  },
+  {
+    "id": "33",
+    "item": "Temaki saumon avocat"
+  },
+  {
+    "id": "34",
+    "item": "Neige avocat (x2)"
+  },
+  {
+    "id": "35",
+    "item": "Neige fromage (x2)"
+  },
+  {
+    "id": "36",
+    "item": "Maki saumon (x2)"
+  },
+  {
+    "id": "37",
+    "item": "Maki thon (x2)"
+  },{
+    "id": "38",
+    "item": "Maki avocat (x2)"
+  },{
+    "id": "39",
+    "item": "Maki concombre (x2)"
+  },
+  {
+    "id": "40",
+    "item": "Maki fromage (x2)"
+  },
+  {
+    "id": "41",
+    "item": "Neige au saumon (x2)"
+  },
+  {
+    "id": "42",
+    "item": "Futomaki (x2)"
+  },
+  {
+    "id": "43",
+    "item": "California surimi avocat (x2)"
+  },
+  {
+    "id": "44",
+    "item": "California comconbre avocat (x2)"
+  },
+  {
+    "id": "45",
+    "item": "California saumon avocat (x2)"
+  },
+  {
+    "id": "46",
+    "item": "California royal (x2)"
+  },
+  {
+    "id": "47",
+    "item": "Maki printemps saumon avocat (x2)"
+  },
+  {
+    "id": "48",
+    "item": "Maki printemps concombre avocat (x2)"
+  },
+  {
+    "id": "49",
+    "item": "Chirachi saumon sur riz vinaigré"
+  },
+  {
+    "id": "50",
+    "item": "Salade au samon"
+  },
+  {
+    "id": "51",
+    "item": "California tempura crevette (x2)"
+  },
+  {
+    "id": "52",
+    "item": "Saumon roll surimi avocat (x2)"
+  },
+  {
+    "id": "53",
+    "item": "California avocat fromage (x2)"
+  },
+  {
+    "id": "55",
+    "item": "Brochette gambas (x3)"
+  },
+  {
+    "id": "56",
+    "item": "Brochette thon (x2)"
+  },
+  {
+    "id": "57",
+    "item": "Tempura beignets de crevettes (x3)"
+  },
+  {
+    "id": "58",
+    "item": "California saumon fromage (x2)"
+  },
+  {
+    "id": "59",
+    "item": "Sashimi saumon"
+  }
+]
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,38 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonInput, IonBadge, IonHeader, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import { menuItems } from '../menuItems';
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Menu</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Menu</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 1 page" />
+
+        <IonList>
+        {menuItems.map(elem => {return(
+          <IonItem>
+
+            <IonBadge color="primary">#{elem["id"]}</IonBadge>
+
+            <IonLabel text-wrap>{elem["item"]}</IonLabel>
+
+            <IonInput style={{ textAlign: 'right' }} type="tel" pattern="[0-9]*" color="primary" ></IonInput>
+
+          </IonItem>
+        )})}
+        </IonList>
+
       </IonContent>
     </IonPage>
   );
